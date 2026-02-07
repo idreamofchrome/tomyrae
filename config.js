9aa180c76cd1714eb386080d8e2905fac3f08c7c
idreamofchrome <soto.alexa773@gmail.com>
2026-02-07 00:23:15 -0800

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        hearts: ['💜', '💖', '💝', '👩🏻‍🤝‍👩🏽', '💓'],  // Heart emojis
+        bears: ['🧸', '🐈']                       // Cute bear emojis
     },
 
     // Questions and answers
